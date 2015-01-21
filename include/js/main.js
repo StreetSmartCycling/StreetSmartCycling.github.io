6fa9f995eac4e5685be75fa1dc240803e644e8b9
sdenee01 <speedygonzalezz22@yahoo.com>
2015-01-21 17:10:29 -0500

    more options commenting plus I replaced the two fancybox js files with the original ones from the template

--- include/js/main.js
+++ include/js/main.js
@@ -131,7 +131,7 @@ BRUSHED.filter = function (){
 			var $optionSet = $this.parents('.option-set');
 			$optionSet.find('.selected').removeClass('selected');
 			$this.addClass('selected');
-			*/
+			
 	  
 			// make option object dynamically, i.e. { filter: '.my-filter-class' }
 			var options = {},
@@ -146,7 +146,7 @@ BRUSHED.filter = function (){
 			} else {
 			  // otherwise, apply new options
 			  $container.isotope( options );
-			}
+			*/
 			
 			return false;
 		});
